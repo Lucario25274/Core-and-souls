0834ad6b2a62c7fbe25e9fd1b029c8e3ee429f7e
Lucario25274 <luca.2527@gmx.de>
2025-06-16 23:47:39 +0200

    combat system erstellt
    hit boxen und animation und attacke für player

--- script/player.gd
+++ script/player.gd
@@ -1,5 +1,13 @@
 extends CharacterBody2D
 
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive =true
+var attack_ip = false #ip= in progress
+
+
+
 const SPEED = 100
 
 var sprite_direction = "right" : get = _get_sprite_direction
@@ -9,27 +17,41 @@ var last_direction: int = 1  # 1 für rechts, -1 für links
 
 @onready var sprite = $AnimatedSprite2D
 
+
 func _physics_process(_delta):
+	enemy_attack()
+	#attack()
+	if health <= 0:
+		player_alive = false  #play end_screen
+		health = 0
+		print("player dead")
+		self.queue_free()
+
+
 	velocity.x = Input.get_axis("xbox_left", "xbox_right") * SPEED
 	velocity.y = Input.get_axis("xbox_up", "xbox_down") * SPEED
 	move_and_slide()
 
-
-	#set_animation("walk_")
 	if velocity.x == 0 and velocity.y == 0:
-		set_animation("idle_")
-		sprite.play("idle_right")
+		if not attack_ip:  # Prüfe, ob attack_ip == false
+			set_animation("idle_")
+			sprite.play("idle_right")
+		else:
+			 #Optional: Animation oder Logik für Angriff, falls attack_ip == true
+			pass
 	else:
 		set_animation("walk_")
+	attack()
 
 
 
 func set_animation(animation):
-#	wenn sprite direction ist up oder dowan dann lass es so
+#	wenn sprite direction ist up oder down dann lass es so
 #	ansonten wenn in left oder right ist, ist es side
 	var direction  = "right" if sprite_direction in ["left","right"] else sprite_direction
 	sprite.play(animation + direction)
 	sprite.flip_h = (sprite_direction == "left")
+
 
 
 func _get_sprite_direction():
@@ -42,3 +64,40 @@ func _get_sprite_direction():
 		sprite_direction = "right"
 
 	return sprite_direction
+
+func player():
+	pass
+
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true :
+		health = health - 10
+		enemy_attack_cooldown = false
+		$attack_colldown.start()
+		print(health)
+
+
+func _on_attack_colldown_timeout():
+	enemy_attack_cooldown = true
+
+
+func attack():
+	if Input.is_action_just_pressed("xbox_attack") and attack_ip == false:
+		attack_ip = true
+		global.player_current_attack = true
+		sprite.play("attack_right")
+		$deal_attack_timer.start()
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	global.player_current_attack = false
+	attack_ip = false
--- script/slime.gd
+++ script/slime.gd
@@ -1,25 +1,35 @@
 extends CharacterBody2D
 
 var speed = 50
-var last_direction: int = 1  # 1 für rechts, -1 für links
+#var last_direction: int = 1  # 1 für rechts, -1 für links
 var player_chase = false
 var player = null
 
+var health = 100
+var player_inattack_range = false
 
-func _physics_process(delta):
-	if player_chase:
-		position += (player.position - position)/speed
+func _physics_process(_delta):
 
-		$AnimatedSprite2D.play("walk")
+	if not health <= 0:
+		if player_chase:
+			position += (player.position - position)/speed
+			$AnimatedSprite2D.play("walk")
 
-		if(player.position.x - position.x) < 0:
-			$AnimatedSprite2D.flip_h = false
+			if(player.position.x - position.x) < 0:
+				$AnimatedSprite2D.flip_h = false
+			else:
+				$AnimatedSprite2D.flip_h = true
 		else:
-			$AnimatedSprite2D.flip_h = true
-
+			$AnimatedSprite2D.play("idle")
 	else:
-		$AnimatedSprite2D.play("idle")
+		print("tot animation")
+		$AnimatedSprite2D.play("death")
+		await $AnimatedSprite2D.animation_finished
+		print("wirklich tot")
+		self.queue_free()
+
 	move_and_slide()
+	deal_with_damage()
 
 
 func _on_detectionarea_body_entered(body):
@@ -28,6 +38,27 @@ func _on_detectionarea_body_entered(body):
 
 
 
-func _on_detectionarea_body_exited(body):
+func _on_detectionarea_body_exited(_body):
 	player = null
 	player_chase = false
+
+func enemy():
+	pass
+
+
+func _on_slime_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_inattack_range = true
+
+
+func _on_slime_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_inattack_range = false
+
+
+func deal_with_damage():
+	if player_inattack_range and global.player_current_attack == true:
+		global.player_current_attack = false
+		health = health - 20
+		self.position += (player.position + position*4)/speed
+		print("slime health = ",health)
